f8f955fcda8a4b13c93d94b947ce55302d0c6a3b
isaac <isaac.brigido@gmail.com>
2024-06-21 12:24:44 -0300

    feat: CRUD ajuste do pagination

---
+++ aula41/ecommerce_desafio_laravel/resources/lang/pt_BR/pagination.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ return [
+    'previous' => '<< Anterior',
+    'next' => 'Próximo >>',
+    'showing' => 'Mostrando',
+    'to' => 'até',
+    'of' => 'de',
+    'results' => 'resultados',
+    ];
+?>
